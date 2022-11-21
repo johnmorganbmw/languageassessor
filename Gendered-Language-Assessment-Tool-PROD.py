fcdf3168e5dccc1339040d0f6c7c5c5a038adb05
johnmorganbmw <114421467+johnmorganbmw@users.noreply.github.com>
2022-11-21 09:56:54 -0500

    replaced altair with plotly, added more comments

--- Gendered-Language-Assessment-Tool-PROD.py
+++ Gendered-Language-Assessment-Tool-PROD.py
@@ -11,22 +11,21 @@ import string
 from io import BytesIO
 import requests
 import en_core_web_sm
-#import altair as alt
 import plotly.graph_objects as go
 
 nlp=en_core_web_sm.load()
 
-#Logo
+###Logo###
 from PIL import Image
 url = 'https://raw.githubusercontent.com/johnmorganbmw/languageassessor/main/IandDLogo_Shrunk.png'
 response = requests.get(url)
 image = Image.open(BytesIO(response.content))
 st.image(image)
 
-#Title
+###Title###
 st.title("Gendered Language Assessor")
 
-#Data Import and preprocessing
+###Data Import and preprocessing###
 url2 = 'https://raw.githubusercontent.com/johnmorganbmw/languageassessor/main/Gendered_Word_Key.csv'
 key = pd.read_csv(url2)
 
@@ -43,7 +42,7 @@ matcher = PhraseMatcher(nlp.vocab) #performs named entity recognition
 matcher.add("MASCULINE",male_nlp) #adds the male terms for the NER
 matcher.add("FEMININE",female_nlp) #adds the female terms for the NER
 
-#Applying the gender recognition to our text
+###Applying the gender recognition to our text###
 job = st.text_area("Copy and paste your text here.") 
 
 job_string = "".join(chain.from_iterable(job)) #converts the copy and pasted text into something understandable
@@ -76,29 +75,16 @@ elif female_count < male_count and male_count - female_count >= 3:
 else: result = "Error!"
 
 st.header("Gendered Language Result")
-result_metric = st.metric(label="",value=result)
+result_metric = st.metric(label="",value=result) #the result from the if statement
 
-#Donut Chart
-#dict = {'Words' : [male_count,female_count], 'Language' : ["Masculine","Feminine"]}
-#result_data = pd.DataFrame(data = dict, index = [0,1])
-
-#base = alt.Chart(result_data).encode(
-   # theta=alt.Theta(field="Words", type="quantitative",stack = True)  
-#)
-
-#donut = base.mark_arc(innerRadius=70).encode(color=alt.Color(field="Language", type="nominal", scale = alt.Scale(domain = ["Masculine","Feminine"], range = ["#fce27a","#66c2c0"])))
-#text = base.mark_text(radius=110, size=30).encode(text="Words")
-
-#final_chart = donut + text
-
-#result_chart = st.altair_chart(final_chart,use_container_width=True)
-
+###Summary Pie Chart###
 colors = ["#fce27a","#66c2c0"]
 
 fig = go.Figure(data=[go.Pie(labels=['Masculine','Feminine'],
                              values=[male_count,female_count])])
 fig.update_traces(hoverinfo='label', textinfo='value', textfont_size=20,
                   marker=dict(colors=colors))
+
 st.plotly_chart(fig, use_container_width=True)
 
 
@@ -107,24 +93,23 @@ st.plotly_chart(fig, use_container_width=True)
 
 st.text("") #blank line to make the app look nicer
 
-#Renders the text with the gendered text flagged
+###Renders the text with the gendered text flagged###
 colors = {"FEMININE" : "#66c2c0", "MASCULINE" : "#fce27a"}
 st.header("Flagged Language")
 visualize_ner(job_nlp, labels= ["MASCULINE","FEMININE"] , displacy_options = {"colors" : colors},title = "",show_table = False)
 
 st.text("") #blank line to make the app look nicer
 
-
-#Text Replacement Recommendation
+###Word Replacement Recommendation###
 st.header("Recommended Word Replacements")
 
-entities = [(e.label_,e.text) for e in job_nlp.ents]
-df_matches = pd.DataFrame(entities, columns=['Language','Word']).query("Language=='MASCULINE'").drop("Language",axis=1)
+entities = [(e.label_,e.text) for e in job_nlp.ents] #pulls out all entities
+df_matches = pd.DataFrame(entities, columns=['Language','Word']).query("Language=='MASCULINE'").drop("Language",axis=1) #saves them as a dataframe and just keeps the masculine words
 
-key2 = key.drop("Female-Coded",axis=1)
-df_merged = df_matches.merge(key2.rename({'Male-Coded': 'Word'}, axis = 1), on = "Word", how = "left").drop_duplicates()
+key2 = key.drop("Female-Coded",axis=1) #creates a key of just the recommendations for masculine words
+df_merged = df_matches.merge(key2.rename({'Male-Coded': 'Word'}, axis = 1), on = "Word", how = "left").drop_duplicates() #joins the matches and recoommended replacements
 
-#sneaking some html in to hide the row index in the below table
+#sneaking some CSS in to hide the row index in the below table
 hide_table_row_index = """ 
             <style>
             thead tr th:first-child {display:none}
@@ -133,4 +118,4 @@ hide_table_row_index = """
             """
 st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
-st.table(df_merged)
+st.table(df_merged) #the recommendations table
